b8a1a5b56753ee409ce7340d7ad7f3328db6bcbe
AlexeyKolesnik94 <86326468+AlexeyKolesnik94@users.noreply.github.com>
2021-11-17 23:26:16 +0300

    added move method

--- Assets/Pirate/Scripts/PlayerController.cs
+++ Assets/Pirate/Scripts/PlayerController.cs
@@ -1,7 +1,30 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-    
-    
-    
+namespace Pirate.Scripts {
+    public class PlayerController : MonoBehaviour {
+
+        [SerializeField] private float speed;
+
+        private Rigidbody2D _rigidbody;
+        private Controls _controls;
+        private float _direction;
+
+        private void Awake() {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _controls = new Controls();
+        }
+
+        private void FixedUpdate() {
+            Move();
+        }
+        
+        private void OnEnable() => _controls.Enable();
+
+        private void OnDisable() => _controls.Disable();
+        
+        private void Move() {
+            _direction = _controls.Player.Move.ReadValue<float>();
+            _rigidbody.velocity = new Vector2(_direction * speed, _rigidbody.velocity.y);
+        }
+    }
 }
