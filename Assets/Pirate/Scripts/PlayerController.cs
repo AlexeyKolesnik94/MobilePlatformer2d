4621b31c7213296bb969c0433365b435b2bd57d7
AlexeyKolesnik94 <86326468+AlexeyKolesnik94@users.noreply.github.com>
2021-11-18 18:09:56 +0300

    "i can flyyyyy...."

--- Assets/Pirate/Scripts/PlayerController.cs
+++ Assets/Pirate/Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ namespace Pirate.Scripts {
     public class PlayerController : MonoBehaviour {
 
         [SerializeField] private float speed;
+        [SerializeField] private float jumpForce;
 
         private Rigidbody2D _rigidbody;
         private Controls _controls;
         private float _direction;
+        private bool _isGround;
+        private bool _isJumping;
 
         private void Awake() {
             _rigidbody = GetComponent<Rigidbody2D>();
@@ -29,9 +32,14 @@ namespace Pirate.Scripts {
             _rigidbody.velocity = new Vector2(_direction * speed, _rigidbody.velocity.y);
         }
         
+        
         private void Jump() {
-            
+            //if (!_isGround) return;
+            _rigidbody.velocity = Vector2.up * jumpForce;
+            _isGround = false;
+            _isJumping = true; 
         }
+        
         
     }
 }
