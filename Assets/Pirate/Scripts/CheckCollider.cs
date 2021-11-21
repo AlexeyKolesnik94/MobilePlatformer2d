5f1ad3294833d57e88db1eebffdc2accd0a8c058
AlexeyKolesnik94 <86326468+AlexeyKolesnik94@users.noreply.github.com>
2021-11-21 23:30:12 +0300

    commit

--- Assets/Pirate/Scripts/CheckCollider.cs
+++ Assets/Pirate/Scripts/CheckCollider.cs
@@ -1,15 +1,29 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Pirate.Scripts {
     public class CheckCollider : MonoBehaviour {
     
+        [SerializeField] private string component;
+        [SerializeField] private EnterEvent action;
+        
         public bool isGround;
     
         private void OnCollisionEnter2D(Collision2D other) {
             Ground ground = other.collider.GetComponent<Ground>();
             if (!ground) return;
-            isGround = true;
+            
+            if (other.collider.GetComponent(component)) {
+                action?.Invoke(other.gameObject);
+            }
+            
         }
-    
+        
+        [Serializable]
+        public class EnterEvent : UnityEvent<GameObject> {
+            
+        }
+        
     }
 }
